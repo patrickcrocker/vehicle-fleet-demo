125d240d790c8facf316dc9baf550efefbf6fb57
bkamysz-pivotal <bkamysz@pivotal.io>
2017-08-25 12:52:52 -0700

    Add dashboard pipeline

---
+++ ci/tasks/build-dashboard/task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+pushd vehicle-fleet-demo/dashboard
+    ./mvnw clean install
+popd
+
+cp vehicle-fleet-demo/dashboard/target/dashboard-1.0.0.BUILD-SNAPSHOT.jar build-output/
+
+cp vehicle-fleet-demo/manifest.yml build-output/
